63bf5a07a791f6ae8c4315d447960c967ee1c669
George <114095068+gappeah@users.noreply.github.com>
2024-03-03 00:38:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import tkinter as tk
-from PIL.Image import core as _imaging
-
 #Creating imports for functions
