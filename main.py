21d545f18ae70d420603541dda9a51d9da7616d5
George <appeahg@outlook.com>
2024-03-08 16:11:49 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 #Serves as the entry point for the project
 #importing libraries in main.py
+# main.py
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QDialog
 from bank_management_system import BankManagementSystem
+from login_screen import LoginScreen
+
+def main():
+    app = QApplication(sys.argv)
+    login_screen = LoginScreen()
+    if login_screen.exec_() == QDialog.Accepted:
+        # Login successful, create and show the main window
+        bank_management_system = BankManagementSystem()
+        bank_management_system.show()
+    else:
+        # Login failed, handle the failure case
+        print("Login failed")
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    bank_management_system = BankManagementSystem()
-    bank_management_system.show()
-    sys.exit(app.exec_())
+    main()
