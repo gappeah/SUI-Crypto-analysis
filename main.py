bd0f7f9c4a8dfcf68eb0147298cf2ba33f31d256
George <appeahg@outlook.com>
2024-03-08 02:30:28 +0000

    imported libraries in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#Creating imports for functions
+#Serves as the entry point for the project
+#importing libraries in main.py
+import sys
+from PyQt5.QtWidgets import QApplication
+from bank_management_system import BankManagementSystem
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    bank_management_system = BankManagementSystem()
+    bank_management_system.show()
+    sys.exit(app.exec_())
